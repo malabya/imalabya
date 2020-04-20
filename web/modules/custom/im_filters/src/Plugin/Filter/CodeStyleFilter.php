8c861fe7acecd35b03e062eeb586f4221303b020
Malabya <malabya35@gmail.com>
2020-04-21 01:50:25 +0530

    Homepage blog listing view.

---
+++ web/modules/custom/im_filters/src/Plugin/Filter/CodeStyleFilter.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Drupal\im_filters\Plugin\Filter;
+
+use Drupal\Component\Utility\Html;
+use Drupal\filter\FilterProcessResult;
+use Drupal\filter\Plugin\FilterBase;
+
+/**
+ * Add code tag structure for syntax highlighting.
+ *
+ * @Filter(
+ *   id = "im_code_style",
+ *   title = @Translation("Code tag for Syntax highlighting"),
+ *   type = Drupal\filter\Plugin\FilterInterface::TYPE_MARKUP_LANGUAGE
+ * )
+ */
+class CodeStyleFilter extends FilterBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function process($text, $langcode) {
+
+    $dom = Html::load($text);
+    $elements = $dom->getElementsByTagName('pre');
+    if ($elements->length === 0) {
+      return new FilterProcessResult(Html::serialize($dom));
+    }
+
+    foreach ($elements as $element) {
+      $content = $element->textContent;
+      $class = $element->getAttribute('class');
+
+      // Create the elements.
+      $code = $dom->createElement('code', $content);
+      $pre = $dom->createElement('pre');
+
+      // Set attributes.
+      $code->setAttribute('class', $class);
+      $pre->setAttribute('class', $class);
+
+      // Append & replace the new element.
+      $pre->appendChild($code);
+      $element->parentNode->replaceChild($pre, $element);
+    }
+
+    $result = new FilterProcessResult(Html::serialize($dom));
+    $result->setAttachments([
+      'library' => 'imalabya/prism',
+    ]);
+
+    return $result;
+  }
+
+}
---
+++ web/modules/custom/im_filters/src/Plugin/Filter/FigureWrapper.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Drupal\im_filters\Plugin\Filter;
+
+use Drupal\Component\Utility\Html;
+use Drupal\filter\FilterProcessResult;
+use Drupal\filter\Plugin\FilterBase;
+
+/**
+ * Add code tag structure for syntax highlighting.
+ *
+ * @Filter(
+ *   id = "im_figure_wrapper",
+ *   title = @Translation("Add a <code>figure</code> wrapper to inline images."),
+ *   type = Drupal\filter\Plugin\FilterInterface::TYPE_MARKUP_LANGUAGE
+ * )
+ */
+class FigureWrapper extends FilterBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function process($text, $langcode) {
+
+    $dom = Html::load($text);
+    $elements = $dom->getElementsByTagName('img');
+    if ($elements->length === 0) {
+      return new FilterProcessResult(Html::serialize($dom));
+    }
+
+    foreach ($elements as $element) {
+      $class = $element->getAttribute('class');
+      $src = $element->getAttribute('src');
+      $alt = $element->getAttribute('alt');
+
+      // Create the elements.
+      $image = $dom->createElement('img');
+      $image->setAttribute('src', $src);
+      $image->setAttribute('alt', $alt);
+      $figure = $dom->createElement('figure');
+
+      // Set attributes.
+      $figure->setAttribute('class', $class);
+
+      // Append & replace the new element.
+      $figure->appendChild($image);
+      $element->parentNode->replaceChild($figure, $element);
+    }
+
+    $result = new FilterProcessResult(Html::serialize($dom));
+
+    return $result;
+  }
+
+}
