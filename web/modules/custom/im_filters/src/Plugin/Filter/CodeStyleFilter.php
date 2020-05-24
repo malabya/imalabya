ca245ce5fad30bf369efe7f416dad70775e06cf7
Malabya <malabya35@gmail.com>
2020-05-24 23:22:22 +0530

    Added line numbers, show language & copy to clipboard functionality for prismjs.

--- web/modules/custom/im_filters/src/Plugin/Filter/CodeStyleFilter.php
+++ web/modules/custom/im_filters/src/Plugin/Filter/CodeStyleFilter.php
@@ -27,15 +27,20 @@ class CodeStyleFilter extends FilterBase {
     if ($elements->length === 0) {
       return new FilterProcessResult(Html::serialize($dom));
     }
+
     foreach ($elements as $element) {
       $content = $element->textContent;
       $class = $element->getAttribute('class');
+
       // Create the elements.
       $code = $dom->createElement('code', htmlentities($content));
       $pre = $dom->createElement('pre');
 
       // Set attributes.
       $code->setAttribute('class', $class);
+
+      // Add the `line-numbers` class for prismjs.
+      $class .= ' line-numbers';
       $pre->setAttribute('class', $class);
 
       // Append & replace the new element.
